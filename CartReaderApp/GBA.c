7d88cb07f922a7ba7fcbb6a04ef00a3aee389142
davidellis1145-web <david.ellis1145@gmail.com>
2026-04-19 23:11:29 -0500

    implement gba spansion fixes, remove comments

--- CartReaderApp/GBA.c
+++ CartReaderApp/GBA.c
@@ -1662,7 +1662,7 @@ void idFlashrom_GBA()
 			if(manufacturerid == 0x1)
 			{
 				// S29GL128N
-				if(strcmp(flashid, "217E") == 0 && romType == 0x1)	// testing
+				if(strcmp(flashid, "217E") == 0 && romType == 0x1)
 				{
 					cartSize = 0x1000000;
 					resetSpansion_GBA();
@@ -1671,8 +1671,8 @@ void idFlashrom_GBA()
 				// S29GL256N
 				if(strcmp(flashid, "227E") == 0 && romType == 0x1)
 				{
-					cartSize = 0x4000000;	// should be 0x2000000, making 0x4000000 for testing
-					resetSpansion_GBA();	// should read 64MB on device
+					cartSize = 0x2000000;
+					resetSpansion_GBA();
 					return;
 				}
 				// S29GL512N
